36100ba44aa7f18fe85882a5ef46edc8c918a6ab
hamdiuludag <hsaruludag@gmail.com>
2025-03-13 00:29:20 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,19 @@
-const CACHE_NAME = "blogger-pwa-cache";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "https://hamdiuludag.github.io/depo/manifest.json",
-  "https://www.1ad.com.tr/favicon.ico",
-  "https://www.1ad.com.tr/favicon.ico"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('blog-cache').then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html'
+            ]);
         })
-      );
-    })
-  );
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
