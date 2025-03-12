7016858e60718b6ee204b40ecf80f1f48545225b
hamdiuludag <hsaruludag@gmail.com>
2025-03-13 00:01:32 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,22 @@
-const CACHE_NAME = "1ad-blog-cache-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "https://www.1ad.com.tr/css/style.css",
-  "https://www.1ad.com.tr/js/script.js",
-  "https://www.1ad.com.tr/images/logo.png"
-];
-
-// Servis çalışanı kurulum ve önbelleğe alma
+// service-worker.js
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
+    caches.open("blog-cache").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "//hamdiuludag.github.io/depo/manifest.json",
+        "/icon-192.png",
+        "/icon-512.png",
+      ]);
     })
   );
 });
 
-// Ağdan veri alma ve önbellekten yükleme
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
-    })
-  );
-});
-
-// Eski önbellekleri temizleme
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
     })
   );
 });
