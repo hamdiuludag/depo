16ff5979b9192228a3e00526814955c81581e406
hamdiuludag <hsaruludag@gmail.com>
2025-03-13 00:20:07 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,38 @@
-const CACHE_NAME = "blog-cache-v1";
+const CACHE_NAME = "blogger-pwa-cache";
 const urlsToCache = [
   "/",
   "/index.html",
-  "/manifest.json"
+  "https://hamdiuludag.github.io/depo/manifest.json",
+  "https://www.1ad.com.tr/favicon.ico",
+  "https://www.1ad.com.tr/favicon.ico"
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
   );
 });
